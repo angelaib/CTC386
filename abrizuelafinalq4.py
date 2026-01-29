9b96084816f6b07882feded4cd7091ffc5599105
angelaib <angelabrizuela@gmail.com>
2026-01-29 02:04:39 +0000

    function, pass user input into function

---
+++ abrizuelafinalq4.py
@@ -0,0 +1,19 @@
+#abrizuela question 4 final exam
+
+#functions
+
+def hny (x):
+    for i in range(x):
+        print("Happy New Year!")
+    return 
+
+
+
+
+#main code
+x = int(input("pick a number between 1 and 10"))
+
+output = hny (x)
+
+
+
